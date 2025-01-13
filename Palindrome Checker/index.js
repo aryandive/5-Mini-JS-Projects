6ce0c71f2c530a9e71ead4056f992193b99b51ff
aryandive <aryandive07@gmail.com>
2025-01-14 01:06:24 +0530

    Initial commit

---
+++ Palindrome Checker/index.js
@@ -0,0 +1,12 @@
+const input = document.getElementById('input1');
+
+function reverseString(str) {
+    return str.split("").reverse();
+    
+} 
+
+function check() {
+    const value = input.value;
+    
+    
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# 5-Mini-JS-Projects
+ I Just completed my coursera js course. Now I am just practicing via making some miniprojects
